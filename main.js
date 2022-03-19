4eb8fb9d84102d23372f1fc8ed164aa733df2a11
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-03-19 08:22:31 +0100

    Update main.js
    when border are touched condition

--- main.js
+++ main.js
@@ -7,4 +7,17 @@ let kx = "+";
 let ky = "+";
 let celerity = 3;
 
-let interval = setInterval(() => {}, 10); 
+let interval = setInterval(() => {
+  if (dvd.offsetLeft + dvd.offsetWidth >= main.offsetWidth) {
+    kx = "-";
+  }
+  if (dvd.offsetLeft - dvd.offsetWidth <= -dvd.offsetWidth) {
+    kx = "+";
+  }
+  if (dvd.offsetTop + dvd.offsetHeight >= main.offsetHeight) {
+    ky = "-";
+  }
+  if (dvd.offsetTop - dvd.offsetHeight <= -dvd.offsetHeight) {
+    ky = "+";
+  }
+}, 10); 
