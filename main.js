2cb733cbbe354ac9126a692979e529113187496b
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-03-19 08:20:16 +0100

    Update main.js
    deleting unused vars and adding celerity var and th interval

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const main = document.querySelector(".main");
 const dvd = document.querySelector(".dvd");
 
-let i = 0;
 let ix = 0;
 let iy = 0;
 let kx = "+";
 let ky = "+";
+let celerity = 3;
+
+let interval = setInterval(() => {}, 10); 
